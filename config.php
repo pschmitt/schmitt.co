a48ac6a19dad4060ec0f6caf4ac5a3d9c9a05c64
Philipp Schmitt <philipp@schmitt.co>
2013-10-26 17:58:42 +0200

    Update theme

--- config.php
+++ config.php
@@ -8,26 +8,26 @@ $config['site_title'] = 'schmitt.co';                 // Site title
 $config['theme'] = 'pico-pure';                       // Set the theme (defaults to "default")
 
 // adv-meta
-$config['adv_meta_values'] = array('description' => 'Description', 'tags' => 'Tags');
+$config['adv_meta_values'] = array('description' => 'Description');
 
 // Google Analytics
 $config['google_tracking_id'] = $ANALYTICS_TRACKING_ID;
 
 /*
 $config['base_url'] = '';                             // Override base URL (e.g. http://example.com)
-$config['date_format'] = 'jS M Y';                // Set the PHP date format
-$config['twig_config'] = array(                        // Twig settings
-        'cache' => false,                                        // To enable Twig caching change this to CACHE_DIR
-        'autoescape' => false,                                // Autoescape Twig vars
-        'debug' => false                                        // Enable Twig debug
+$config['date_format'] = 'jS M Y';                    // Set the PHP date format
+$config['twig_config'] = array(                       // Twig settings
+        'cache' => false,                             // To enable Twig caching change this to CACHE_DIR
+        'autoescape' => false,                        // Autoescape Twig vars
+        'debug' => false                              // Enable Twig debug
 );
-$config['pages_order_by'] = 'alpha';        // Order pages by "alpha" or "date"
-$config['pages_order'] = 'asc';                        // Order pages "asc" or "desc"
-$config['excerpt_length'] = 50;                        // The pages excerpt length (in words)
+$config['pages_order_by'] = 'alpha';                  // Order pages by "alpha" or "date"
+$config['pages_order'] = 'asc';                       // Order pages "asc" or "desc"
+$config['excerpt_length'] = 50;                       // The pages excerpt length (in words)
 
 // To add a custom config setting:
 
-$config['custom_setting'] = 'Hello';         // Can be accessed by {{ config.custom_setting }} in a theme
+$config['custom_setting'] = 'Hello';                  // Can be accessed by {{ config.custom_setting }} in a theme
 
 */
 
