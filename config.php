3f8ce82663c8d9aa08c61538107dff0f6254f9ab
Philipp Schmitt <philipp@schmitt.co>
2013-10-26 22:18:16 +0200

    Changed order (date|desc) 	modified:   config.php

--- config.php
+++ config.php
@@ -6,6 +6,8 @@ include '../paasswd/analytics-key.inc.php';
 
 $config['site_title'] = 'schmitt.co';                 // Site title
 $config['theme'] = 'pico-pure';                       // Set the theme (defaults to "default")
+$config['pages_order_by'] = 'date ';                  // Order pages by "alpha" or "date"
+$config['pages_order'] = 'desc';                      // Order pages "asc" or "desc"
 
 // adv-meta
 $config['adv_meta_values'] = array('description' => 'Description');
@@ -21,8 +23,6 @@ $config['twig_config'] = array(                       // Twig settings
         'autoescape' => false,                        // Autoescape Twig vars
         'debug' => false                              // Enable Twig debug
 );
-$config['pages_order_by'] = 'alpha';                  // Order pages by "alpha" or "date"
-$config['pages_order'] = 'asc';                       // Order pages "asc" or "desc"
 $config['excerpt_length'] = 50;                       // The pages excerpt length (in words)
 
 // To add a custom config setting:
