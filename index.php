ae294a0072069867ac96e0ff5b28126e98945bc6
Philipp Schmitt <philipp@schmitt.co>
2014-10-04 16:16:38 +0200

    Don't copy Phile's index.php but include it with our ROOT_DIR set

--- index.php
+++ index.php
@@ -1,37 +1,6 @@
 <?php
 
-/**
- * @author Frank Nägler
- * @link https://philecms.com
- * @license http://opensource.org/licenses/MIT
- * @package Phile
- */
-
 // overwrite rootdir since phile resides in phile/ and not the root
 define('ROOT_DIR', __DIR__ . DIRECTORY_SEPARATOR);
 
-require_once __DIR__ . '/lib/Phile/Bootstrap.php';
-
-ob_start();
-
-try {
-	$boostrap = \Phile\Bootstrap::getInstance()->initializeBasics();
-	$phileCore = new \Phile\Core($boostrap);
-	echo $phileCore->render();
-} catch (\Phile\Exception $e) {
-	if (\Phile\ServiceLocator::hasService('Phile_ErrorHandler')) {
-		ob_end_clean();
-
-		/** @var \Phile\ServiceLocator\ErrorHandlerInterface $errorHandler */
-		$errorHandler = \Phile\ServiceLocator::getService('Phile_ErrorHandler');
-		$errorHandler->handleException($e);
-	}
-} catch (\Exception $e) {
-	if (\Phile\ServiceLocator::hasService('Phile_ErrorHandler')) {
-		ob_end_clean();
-
-		/** @var \Phile\ServiceLocator\ErrorHandlerInterface $errorHandler */
-		$errorHandler = \Phile\ServiceLocator::getService('Phile_ErrorHandler');
-		$errorHandler->handleException($e);
-	}
-}
+require_once ROOT_DIR . 'phile' . DIRECTORY_SEPARATOR . 'index.php';
